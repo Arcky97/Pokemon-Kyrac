23456af48f9c71299daf299f935754fe7f4806d7
Arcky97 <syntixis@gmail.com>
2023-01-10 17:31:02 +0100

    better region map

--- Plugins/Arcky's Custom Scripts/Rival Battles.rb
+++ Plugins/Arcky's Custom Scripts/Rival Battles.rb
@@ -2,11 +2,7 @@
 def pbRivalDoubleBattle(name,nr)
     $MidBattle = [nil,:RIVAL_BATTLE_LAB]
     starterchoice = $game_variables[7]
-    if $game_switches[96] == true
-        rival = [:RIVAL1_F,:RIVAL2_M]
-    else
-        rival = [:RIVAL1_M,:RIVAL2_F]
-    end
+    rival = $game_switches[96] ? [:RIVAL1_F,:RIVAL2_M] : [:RIVAL1_M,:RIVAL2_F]
     TrainerBattle.dx_start([rival[0],name,starterchoice,rival[1],name,starterchoice],{:canlose => true, :outcome => 29},$MidBattle[nr])
 end
 #this method is only called when one of the Rivals is battled
